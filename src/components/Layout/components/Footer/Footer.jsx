d0d069b1ca033d2608ebc02ca2be068960bd2bbb
Nikola Mirilo <nikolamirilo@gmail.com>
2022-10-08 15:45:10 +0200

    added content to navbar and footer

--- src/components/Layout/components/Footer/Footer.jsx
+++ src/components/Layout/components/Footer/Footer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer className="footer"></footer>;
+  return (
+    <footer className="footer">
+      <p>Copyright @S2S-2022</p>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/Layout/components/Navbar/Navbar.jsx
+++ src/components/Layout/components/Navbar/Navbar.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import logo from "../../../../assets/images/Logo.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <nav className="navbar"></nav>;
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <img src={logo} alt="Logo" />
+      </div>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/">Contact</Link>
+        <Link to="/">Images</Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
